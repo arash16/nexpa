be5e556fb531aef548eb09b5a41615c1194856bf
Arash <arash.shakery@gmail.com>
2018-02-20 19:39:44 +0330

    customResolveOptions.pathFilter for rollup

--- test-project/rollup.config.js
+++ test-project/rollup.config.js
@@ -1,5 +1,6 @@
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
+import path from 'path';
 
 export default {
     input: 'src/index.js',
@@ -13,7 +14,22 @@ export default {
         chokidar: true
     },
     plugins: [
-        resolve(),
+        resolve({
+            customResolveOptions: {
+                // for sub modules inside es6 compiled modules, look inside main/module file's folder
+                pathFilter: function (pkg, fullPath, relativePath) {
+                    let indexDir = pkg.module || pkg['jsnext:main'] || pkg.browser || pkg.main;
+                    if (indexDir) {
+                        if (/\.js$/.test(indexDir))
+                            indexDir = path.dirname(indexDir);
+
+                        if (!relativePath.startsWith(indexDir))
+                            relativePath = path.join(indexDir, relativePath);
+                    }
+                    return relativePath;
+                }
+            }
+        }),
         babel({
             externalHelpers: false,
             exclude: 'node_modules/**'
