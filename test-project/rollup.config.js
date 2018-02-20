fcca18c5e5f90c2f7859d7241a98ed7384519afc
Arash <arash.shakery@gmail.com>
2018-02-20 20:05:35 +0330

    customResolveOptions.pathFilter inside separate npm module

--- test-project/rollup.config.js
+++ test-project/rollup.config.js
@@ -1,6 +1,5 @@
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
-import path from 'path';
 
 export default {
     input: 'src/index.js',
@@ -17,17 +16,7 @@ export default {
         resolve({
             customResolveOptions: {
                 // for sub modules inside es6 compiled modules, look inside main/module file's folder
-                pathFilter: function (pkg, fullPath, relativePath) {
-                    let indexDir = pkg.module || pkg['jsnext:main'] || pkg.browser || pkg.main;
-                    if (indexDir) {
-                        if (/\.js$/.test(indexDir))
-                            indexDir = path.dirname(indexDir);
-
-                        if (!relativePath.startsWith(indexDir))
-                            relativePath = path.join(indexDir, relativePath);
-                    }
-                    return relativePath;
-                }
+                pathFilter: require('resolve-submodule-filter')
             }
         }),
         babel({
